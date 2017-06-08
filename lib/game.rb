1e7d87d9e52a13caeb17d03ac4814ea312811960
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-08 11:11:49 -0600

    fix spacing

--- lib/game.rb
+++ lib/game.rb
@@ -12,38 +12,37 @@ class Game
     @message                       = Message.new
   end
 
-	def play_game
-		user_input = ""
-		while user_input != "q"
+  def play_game
+    user_input = ""
+    while user_input != "q"
       system "clear"
-			puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-			user_input = gets.chomp
-			if user_input == "i"
-				puts "These are the instructions for how to play battleship."
-			elsif user_input == "p"
-				ship_layout_phase
-			elsif user_input == "q"
-				puts "You have chosen to quit BATTLESHIP. Thank you for playing!"
-			else
-				puts "#{user_input} is an invalid command."
-			end
-		end
-	end
+      puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+      user_input = gets.chomp
+        if user_input == "i"
+          puts "These are the instructions for how to play battleship."
+        elsif user_input == "p"
+          ship_layout_phase
+        elsif user_input == "q"
+          puts "You have chosen to quit BATTLESHIP. Thank you for playing!"
+        else
+          puts "#{user_input} is an invalid command."
+        end
+    end
+  end
 
-	def ship_layout_phase
+  def ship_layout_phase
     system "clear"
     @message.ship_layout_phase_message
     first_ship = gets.chomp
-		@players_grid.place_ship(1, first_ship)
+    @players_grid.place_ship(1, first_ship)
     system "clear"
     p "Enter the squares for your three-unit ship, like this Y1 Y2 Y3:"
     second_ship = gets.chomp
     @players_grid.place_ship(2, second_ship)
     @computers_grid.place_ship(1, "A1 A2")
     @computers_grid.place_ship(2, "C5 D5 E5")
-
     battle_phase
-	end
+  end
 
   def battle_phase
     @players_grid.print_board
@@ -51,11 +50,11 @@ class Game
     p "Attack your opponent! Choose a square coordinate where to shoot, like this W1:"
     users_input = gets.chomp.split('')
     outcome = @computers_grid.receive_shot(users_input[0], users_input[1].to_i)
-    if outcome.is_a? Numeric
-      @players_view_of_computers_grid.set_a_hit(users_input[0], users_input[1].to_i)
-    else
-      @players_view_of_computers_grid.set_a_miss(users_input[0], users_input[1].to_i)
-    end
+      if outcome.is_a? Numeric
+        @players_view_of_computers_grid.set_a_hit(users_input[0], users_input[1].to_i)
+      else
+        @players_view_of_computers_grid.set_a_miss(users_input[0], users_input[1].to_i)
+      end
     @computers_grid = Board.new
     @computers_grid.set_a_miss(users_input[0], users_input[1].to_i)
     @computers_grid.print_board
